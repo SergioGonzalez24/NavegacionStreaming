2128849a0e9e9c607567f3a7899d4d8623af7625
André <84719490+gggandre@users.noreply.github.com>
2022-09-05 15:24:49 -0500

    cambios

--- app/src/main/java/mx/itesm/ag/navegacionstreaming/CostoFrag.kt
+++ app/src/main/java/mx/itesm/ag/navegacionstreaming/CostoFrag.kt
@@ -7,18 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 
 class CostoFrag : Fragment() {
 
     private val viewModel: CostoViewModel by viewModels()
+    //argumentos
+    private val args: CostoFragArgs by navArgs()
     //binding
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_costo, container, false)
+    }
+    //Pantalla ya es visible, pero la gui no es interactiva todavía
+    override fun onStart() {
+        super.onStart()
+        println("${args.tipoServicio}")
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
 
