f46c90133a6269f53cfb77c13a2f4a70541845d7
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2022-09-13 14:47:49 -0500

    .

--- app/src/main/java/mx/itesm/ag/navegacionstreaming/CostoFrag.kt
+++ app/src/main/java/mx/itesm/ag/navegacionstreaming/CostoFrag.kt
@@ -12,20 +12,16 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import mx.itesm.ag.navegacionstreaming.databinding.FragmentCostoBinding
-import android.widget.Toast
 
 class CostoFrag : Fragment() {
 
-    /**
-     * @author Gilberto André García Gaytán
-     * Este script descarga los datos y servicios a mostrar en la app
-     */
-
     private val viewModel: CostoViewModel by viewModels()
+    //argumentos
     private val args: CostoFragArgs by navArgs()
+    //binding
     private lateinit var binding: FragmentCostoBinding
-    //Costo variable
-    private  var costo = 0.0
+    //Costo
+    private var costo = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,38 +30,40 @@ class CostoFrag : Fragment() {
         binding = FragmentCostoBinding.inflate(layoutInflater)
         return binding.root
     }
-
-    /*Cuando la GUI ya es visible en la pantalla (pero los componentes
- aún no pueden interactuar con el usuario)*/
+    //Pantalla ya es visible, pero la gui no es interactiva todavía
     override fun onStart() {
         super.onStart()
-        println("Servicio seleccionado: ${args.tipoServicio}")
-        registrarObservadores()
+        println("${args.tipoServicio}")
         registrarEventos()
+        configurarObservadores()
+        viewModel.calcularCosto(args.tipoServicio)
     }
 
     private fun registrarEventos() {
+        // Cancelar u Confirmar
         binding.btnCancelar.setOnClickListener {
             costo = 0.0
-            regresar()
+            setFragmentResult("calcularCosto", bundleOf("costo" to costo))
+            findNavController().navigateUp()
         }
         binding.btnConfirmar.setOnClickListener {
-            regresar()
+            setFragmentResult("calcularCosto", bundleOf("costo" to costo))
+            findNavController().navigateUp()
         }
+
     }
 
-    private fun regresar() {
-        setFragmentResult("calcularCosto", bundleOf("costo" to costo))
-        findNavController().navigateUp()
-    }
-
-    private fun registrarObservadores() {
-        viewModel.descargarCosto(args.tipoServicio)
+    private fun configurarObservadores() {
         viewModel.costo.observe(viewLifecycleOwner){ costo ->
+            //manda donde esta viniendo el viewlifecycleowner
             this.costo = costo
-            binding.tvCosto.setText("El costo de ${args.tipoServicio} es $" + "%.2f".format(costo))
+            binding.tvCosto.setText("El costo del tipo de ${args.tipoServicio} es $" + "%.2f".format(costo))
         }
-        viewModel.descargarCosto(args.tipoServicio)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
 }
